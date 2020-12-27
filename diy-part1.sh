2d9b896b2fc7894be5b8f9755fde28407d1fa485
hungleungi <75869855+hungleungi@users.noreply.github.com>
2020-12-27 15:28:05 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,6 +14,6 @@
 sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
-sed -i '$a src-git official https://github.com/openwrt/packages' feeds.conf.default
-sed -i '$a src-git passwall https://github.com/hungleungi/openwrt-passwall' feeds.conf.default
+#sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+#sed -i '$a src-git official https://github.com/openwrt/packages' feeds.conf.default
+#sed -i '$a src-git passwall https://github.com/hungleungi/openwrt-passwall' feeds.conf.default
