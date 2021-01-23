1164e5108e4d6bdbb3cf6629b5edbd35f15c551d
hungleungi <75869855+hungleungi@users.noreply.github.com>
2021-01-23 23:00:22 +0800

    Create official-n1.sh

---
+++ official-n1.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#
+# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part2.sh
+# Description: OpenWrt DIY script part 2 (After Update feeds)
+#
+git clone https://github.com/hungleungi/openwrt-passwall package/openwrt-passwall
+git clone https://github.com/rufengsuixing/luci-app-adguardhome package/luci-app-adguardhome
+git clone https://github.com/tuanqing/install-program package/install-program
+git clone https://github.com/pymumu/luci-app-smartdns package/luci-app-smartdns
+git clone https://github.com/pymumu/openwrt-smartdns package/openwrt-smartdns
+# Modify default IP
+sed -i 's/192.168.1.1/192.168.10.252/g' package/base-files/files/bin/config_generate
