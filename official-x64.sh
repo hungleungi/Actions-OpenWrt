927115e045fc12524d79ba60733c386b7fe40f2f
hungleungi <75869855+hungleungi@users.noreply.github.com>
2021-01-27 11:29:54 +0800

    Update official-x64.sh

--- official-x64.sh
+++ official-x64.sh
@@ -9,14 +9,20 @@
 # File name: diy-part2.sh
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
-git clone https://github.com/yichya/openwrt-xray package/openwrt-xray
+#git clone https://github.com/yichya/openwrt-xray package/openwrt-xray
 git clone https://github.com/yichya/luci-app-xray package/luci-app-xray
-git clone https://github.com/mchome/luci-app-vlmcsd package/luci-app-vlmcsd
+
 git clone https://github.com/mchome/openwrt-vlmcsd package/openwrt-vlmcsd
+git clone https://github.com/mchome/luci-app-vlmcsd package/luci-app-vlmcsd
+
 git clone https://github.com/hungleungi/openwrt-passwall package/openwrt-passwall
-git clone https://github.com/rufengsuixing/luci-app-adguardhome package/luci-app-adguardhome
+git clone https://github.com/hungleungi/luci-app-passwall package/luci-app-passwall
+
 git clone https://github.com/AdguardTeam/AdGuardHome package/AdGuardHome
+git clone https://github.com/rufengsuixing/luci-app-adguardhome package/luci-app-adguardhome
+
+#git clone https://github.com/pymumu/openwrt-smartdns package/openwrt-smartdns
 git clone https://github.com/pymumu/luci-app-smartdns package/luci-app-smartdns
-git clone https://github.com/pymumu/openwrt-smartdns package/openwrt-smartdns
+
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.10.252/g' package/base-files/files/bin/config_generate
